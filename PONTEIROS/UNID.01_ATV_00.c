d11e714ef2b1e93b43a0710c4474ff921a8a3fa7
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2024-01-11 15:30:51 -0400

    CORRIGINDO

--- PONTEIROS/UNID.01_ATV_00.c
+++ PONTEIROS/UNID.01_ATV_00.c
@@ -3,43 +3,42 @@ Executar pelo terminal: g++ -o UNID.01_ATV_00 UNID.01_ATV_00.c
 Depois: ./UNID.00_ATV_00
 */
 
-// EM CORREÇÃO !
-
 #include <stdlib.h>
 #include <stdio.h>
 
-char genero (char* sexo, int year){
+void genero (int *sexo, int *idade){
+
     
-    if(*sexo == 'F' ){
-       
-       if (year < 60){
-        printf("Se aposentará em: %d anos", 60 - year);
-       }
-       else{
-        printf("Já pode se aposentar");
-       }
+    if(*sexo ==  1 ){
+       printf("Sexo feminino \n");
+        if (*idade < 60){
+            printf("Se aposentará em: %d anos", 60 - *idade);
+        }
+        else{
+            printf("Já pode se aposentar");
+        }
     }
     else {
-       
-       if (year < 65) {
-        printf("Se aposentará em: %d anos", 65 - year);
-       }
-       else{
+       if(*sexo == 2){
+        printf("Sexo masculino \n");
+        
+        if (*idade < 65) {
+        printf("Se aposentará em: %d anos", 65 - *idade);
+        }
+        else{
         printf("Já pode se aposentar");
-       }
+        }
+        }
     }
 }
 
-int main(int argc, char** argv){
-    
-    int year;
-    char sexo[15];
+int main(int sex, int id){
     
     printf("Quantos anos vc tem ? ");
-    scanf("%d", &year);
-    
+    scanf("%d", &id);
+    printf("Digite 1 para Feminino ou 2 para Masculino \n"); 
     printf("Qual o seu sexo ? ");
-    scanf("%s", &sexo);
+    scanf("%d", &sex);
     
-    genero (sexo, year);
+    genero (&sex, &id);
 }
