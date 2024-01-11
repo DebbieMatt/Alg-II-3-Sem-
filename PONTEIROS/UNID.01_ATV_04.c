b875870ed9cff8be84dc08c346356a4596a1d953
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2024-01-11 17:40:24 -0400

    Update UNID.01_ATV_04.c

--- PONTEIROS/UNID.01_ATV_04.c
+++ PONTEIROS/UNID.01_ATV_04.c
@@ -1,13 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
+void imprimir(int a[], int b[]){
     
+    // imprimindo 
+    
+    printf("Vetor A: \n");
+    
+    for(int i = 0; i < 2; i++){
+        printf("[%d] ", *(a + i));
+    }
+    printf("\nVetor B: \n");
+    
+    for(int j = 0; j < 2; j++){
+        printf("[%d] ", *(b + j));
+    }
+}
+
+int main(){
+
     int a[2], b[2];
     
-    // declara um ponteiro para uma variável do tipo int
-    int *p, *q;
-  
     // preenchendo
     
     for(int i = 0; i < 2; i++){
@@ -18,34 +31,6 @@ int main(){
         printf("Digite um numero: \n");
         scanf("%d", &b[j]);
     }
-    
-    // aponta para a variável "a" e altera seu valor
-    
-        p = &a[0];
-        *p = b[0];
-        
-        p = &a[1];
-        *p = b[1];
-        
-    // imprimindo 
-    
-    printf("Vetor A: \n");
-    for(int i = 0; i < 2; i++){
-        printf("[%d] ", a[i]);
-    }
-    
-    // aponta para a variável "b" e altera seu valor
-    
-        q = &b[1]; // PARA CORRIGIR 
-        *q = a[1]; // " "
-        
-        q = &a[0]; // PARA CORRIGIR
-        *q = b[0]; // " "
-    
-    // imprimindo
-    
-    printf("\nVetor B: \n");
-    for(int j = 0; j < 2; j++){
-        printf("[%d] ", b[j]);
-    }
+
+    imprimir(a, b);
 }
