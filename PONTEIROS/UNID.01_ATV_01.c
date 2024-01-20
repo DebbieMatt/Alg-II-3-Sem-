921632d73bab58fa711e76d0b669bd5f20620ff6
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2024-01-19 22:51:33 -0400

    Update UNID.01_ATV_01.c

--- PONTEIROS/UNID.01_ATV_01.c
+++ PONTEIROS/UNID.01_ATV_01.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	int a;
-	float b;
-	
-	a = 10;
-	b = 15.8;
-	
-	if (&a > &b) 
-		printf("\nO endereco de a: %d (%p) e maior que o endereco de b: %.1f (%p).\n\n", a, &a, b, &b);
+
+void compare(int a, float b) {
+	if ((float*)&a > &b) 
+		printf("\nO endereco de a: %d (%p) é maior que o endereco de b: %.1f (%p).\n\n", a, &a, b, &b);
 	else 
-		printf("\nO endereco de b: %.1f (%p) e maior que o endereco de a: %d (%p).\n\n", b, &b, a, &a);
-		
-	return 0;
+		printf("\nO endereco de b: %.1f (%p) é maior que o endereco de a: %d (%p).\n\n", b, &b, a, &a);
+}
+void main(int a, float b){
+    printf("\nDigite um inteiro: ");
+    scanf("%d", &a);
+    printf("\nDigite um flutuante: ");
+    scanf("%f", &b);
+    compare(a, b);
 }
