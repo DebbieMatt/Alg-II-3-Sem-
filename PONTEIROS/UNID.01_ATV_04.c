2b809813961e2f9929de2947287335964f5f299c
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2024-01-23 16:56:08 -0400

    CORRIGIDO

--- PONTEIROS/UNID.01_ATV_04.c
+++ PONTEIROS/UNID.01_ATV_04.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void imprimir(int a[], int b[]){
+void inverter(int *a, int *b){
     
-    // imprimindo 
+    int aux[2];
     
-    printf("Vetor A: \n");
-    
-    for(int i = 0; i < 2; i++){
-        printf("[%d] ", *(a + i));
+    for (int i = 0; i < 2; i++){
+      aux[i] = *( a + i);
     }
-    printf("\nVetor B: \n");
-    
-    for(int j = 0; j < 2; j++){
-        printf("[%d] ", *(b + j));
+    for (int i = 0; i < 2; i++){
+      *(a + i) = *(b + i);
+    }
+    for (int i = 0; i < 2; i++){
+      *(b + i) = aux[i];
     }
 }
 
-int main(){
+void main(){
 
     int a[2], b[2];
-    
+
     // preenchendo
-    
+
     for(int i = 0; i < 2; i++){
         printf("Digite um numero: \n");
         scanf("%d", &a[i]);
@@ -32,5 +31,18 @@ int main(){
         scanf("%d", &b[j]);
     }
 
-    imprimir(a, b);
+    inverter(a,b);
+
+    // imprimindo 
+
+    printf("Vetor A: \n");
+
+    for(int i = 0; i < 2; i++){
+        printf("[%d] ", *(a + i));
+    }
+    printf("\nVetor B: \n");
+
+    for(int j = 0; j < 2; j++){
+        printf("[%d] ", *(b + j));
+    }
 }
