ec932aefd31a65b0ba1d8d8175260461f53ebbe8
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2024-01-23 19:18:04 -0400

    Update UNID.01_ATV_03.c

--- PONTEIROS/UNID.01_ATV_03.c
+++ PONTEIROS/UNID.01_ATV_03.c
@@ -1,47 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(){
     
     int dep = 0, juros = 1;
-    double money_func = 0.0, money_atual = 0.0;
-    double *p;
+    double money_func = 0.0, money_atual = 0.0, p = 0.0;
+
+    do{
+        
+        printf("\n(Digite -1, para parar).\n");
+        printf("\nQual departamento pertence ? ");
+        scanf("%d", &dep);
+          
+        printf("\nQual salario atual? ");
+        scanf("%lf", &money_func);
+        
+      
+        if(dep == 1 || dep == 2){
+        
+            p = money_func;
+            money_atual = money_func * 0.22;
+            p = money_func + money_atual;
+        
+        printf("\nDepartamento %d, recebera R$ %.2lf \n", dep,p);
+        }
+        else if(dep == 3){
+        
+            p = money_func;
+            money_atual = money_func * 0.17;
+            p = money_func + money_atual;
+        
+        printf("\n Departamento %d, recebera R$ %.2lf \n", dep, p);
+        }
+        else if(dep == 4 || dep == 5){
+        
+            p = money_func;
+            money_atual = money_func * 0.10;
+            p = money_func + money_atual;
+        
+        printf("\nDepartamento %d, recebera R$ %.2lf \n", dep, p);
+        }
+      
+        else{
+            printf("Codigo invalido.\n");
+        }
+    }while (dep != -1);
     
-    printf("Qual departamento vc pertence ? \n");
-    scanf("%d", &dep);
-    
-    printf("Quanto vc recebe ? \n");
-    scanf("%lf", &money_func);
-
-    switch (dep){
-
-    case 1 ... 2:
-        
-        p = &money_func;
-        money_atual = money_func * 0.22;
-        *p = money_func + money_atual;
-        
-        printf("\n Por ser do departamento 1 ou 2, seu salario passará a ser R$ %.2lf. \n", money_func);
-    break;
-
-    case 3:
-        p = &money_func;
-        money_atual = money_func * 0.17;
-        *p = money_func + money_atual;
-        
-        printf("\n Por ser do departamento %d, seu salario passará a ser R$ %.2lf. \n", dep, money_func);
-    break;
-
-    case 4 ... 5:
-        p = &money_func;
-        money_atual = money_func * 0.10;
-        *p = money_func + money_atual;
-        
-        printf("\n Por ser do departamento 4 ou 5, seu salario passará a ser R$ %.2lf. \n", money_func);
-    break;
-
-    default:
-        printf("codigo invalido");
-    break;
-    }
 }
