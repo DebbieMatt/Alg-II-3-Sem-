6dcdfd8d708a1f3901d246b0a582fce8e29231a3
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2024-01-23 19:04:08 -0400

    Update UNID.01_ATV_05.c

--- PONTEIROS/UNID.01_ATV_05.c
+++ PONTEIROS/UNID.01_ATV_05.c
@@ -1,47 +1,52 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int receberVetor(int vetA[], int vetB[], int vetC[]){ // recebendo vetor
-
-int maior = 0;
-
-    printf("Primeiro elemento de A: %d\n", *vetA);
-    printf("Segundo elemento de A: %d\n", *(vetA+1));
-    printf("\n");
-    printf("Primeiro elemento de B: %d\n", *vetB);
-    printf("Segundo elemento de B: %d\n", *(vetB+1));
+void inverter(int *a, int *b, int *c){
     
-    
-    
-    for(int i = 0; i < 2; i++){
-               
-        if(maior < *vetA){
+    for (int i = 0; i < 4; i++){
+        
+        if (a < b){
+            *(c + i) = *( a + i);
             
-            vetC[i] = maior;
+        }else{
+            *(c + i) = *( b + i);
+            
         }
     }
-
-    printf("Primeiro elemento de C: %d\n", *vetC);
-    printf("Segundo elemento de C: %d\n", *(vetC+1));
 }
 
-
 int main(){
-    
-    int vetA[2], vetB[2], vetC[2];
-    
+
+    int a[2], b[2], c[2];
+
+    // preenchendo
+
     for(int i = 0; i < 2; i++){
-        
-            printf(" vetA[%d]:", i);
-            scanf("%d ", &vetA[i]);
+        printf("Digite um numero: \n");
+        scanf("%d", &a[i]);
     }
-    
+    for(int j = 0; j < 2; j++){
+        printf("Digite um numero: \n");
+        scanf("%d", &b[j]);
+    }
+
+    inverter(a,b,c);
+
+    // imprimindo 
+
+    printf("\nVetor A: \n");
+
     for(int i = 0; i < 2; i++){
-        
-            printf(" vetB[%d]:", i);
-            scanf("%d ", &vetB[i]);
+        printf("[%d] ", *(a + i));
     }
-    
-    printf("\n");
-    
-    receberVetor(vetA, vetB, vetC);
+    printf("\nVetor B: \n");
+
+    for(int j = 0; j < 2; j++){
+        printf("[%d] ", *(b + j));
+    }
+    printf("\nVetor C: \n");
+
+    for(int k = 0; k < 2; k++){
+        printf("[%d] ", *(c + k));
+    }
 }
